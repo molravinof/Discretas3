c53d97567cf395240c9fa1d1759034965e562fe2
molravinof <mol.ravinof@gmail.com>
2017-05-18 22:02:36 -0500

    Dijkstra ejemplo de seteo de bits

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -1,7 +1,6 @@
 #include<NTL/ZZ.h>
-//#include<iostream>
-//#include<omp.h>
-//#include<cstdio>
+#include<omp.h>
+#include<cstdio>
 using namespace std;
 using namespace NTL;
 
@@ -9,34 +8,37 @@ typedef ZZ nat;
 
 nat mcd(nat &a,nat &b){
     //cout<<"a: "<<a<<" b: "<<b<<endl;
-    if(a==0 || b==0)
-        return a+b;
+    if(a==b)
+        return a;
     else if(a>b){
-        a%=b;
+        a-=b;
         return mcd(a,b);
     }
     else if(b>a){
-        b%=a;
+        b-=a;
         return mcd(a,b);
     }
 }
 int main(){
-   // printf("-------- Simply count elapsed time (CountTime) -------\n");
-   // const double startTime = omp_get_wtime();
+    printf("-------- Simply count elapsed time (CountTime) -------\n");
+    const double startTime = omp_get_wtime();
     nat a,b,c;
-    a=1844674407370955161;
-    b=8388607;
+    a=9223372036854775800;
+    b=4540737095516151000;
     int k,q;
-    k=NumBits(a);
+    SetBit(a,63); //seteamos los bits de a 63 aunq sale despues 64
+    SetBit(b,63); 
+    k=NumBits(a); //verificamos con esta funcion los bits q se estan usando
     q=NumBits(b);
     c=mcd(a,b);
     cout<<"mcd: "<<c<<endl;
-    cout<<k<<"\n"<<q<<endl;
-    cout<<sizeof(a)<<endl;
-   // const double endTime = omp_get_wtime();
-   // printf("Duration = %lf seconds\n", (endTime - startTime));
-   // printf("------------------------------------------------------\n");
+    cout<<"los bits de a: "<<k<<"\n"<<"los bits de b: "<<q<<endl;
+    cout<<"los bytes de a: "<<sizeof(a)<<endl;
+    cout<<"los bytes de b: "<<sizeof(b)<<endl;
+    const double endTime = omp_get_wtime();
+    printf("Duration = %lf seconds\n", (endTime - startTime));
+    printf("------------------------------------------------------\n");
     return 0;
 }
 
-//g++ -g -O2 dijzzz.cpp -lgomp -o m -lntl -lgmp -lm
+//g++ -g -O2 dijkstra.cpp -lgomp -o m -lntl -lgmp -lm
